46126326ddbd1db1f8870d6093720689797e63bf
Vivek Rai <vkkr1430@gmail.com>
2025-11-13 09:34:11 +0530

    code changes

--- app.js
+++ app.js
@@ -114,6 +114,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", {err});
 })
 
-app.listen(8080, () => {
+app.listen(3000, () => {
     console.log("app is listening to port 8080");
 })
